69557823502a46e6a65f46bc04583bc3b849f13c
SuchitReddi <suchit20016@gmail.com>
2023-11-14 12:10:01 +0530

    Moved Create database button to top in setup.php

--- setup.php
+++ setup.php
@@ -49,12 +49,17 @@ $page[ 'body' ] .= "
 
 	<p>Click on the 'Create / Reset Database' button below to create or reset your database.<br />
 	If you get an error make sure you have the correct user credentials in: <em>" . realpath(  getcwd() . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "config.inc.php" ) . "</em></p>
-
+	
 	<p>If the database already exists, <em>it will be cleared and the data will be reset</em>.<br />
 	You can also use this to reset the administrator credentials (\"<em>admin</em> // <em>password</em>\") at any stage.</p>
+	<!-- Create db button -->
+	<form action=\"#\" method=\"post\">
+		<input name=\"create_db\" type=\"submit\" value=\"Create / Reset Database\">
+		" . tokenField() . "
+	</form>
+	<br />
 	<hr />
 	<br />
-
 	<h2>Setup Check</h2>
 	<br>
 	{$SERVER_NAME}<br />
@@ -94,13 +99,6 @@ allow_url_include = On</code></pre>
 	These are only required for the file inclusion labs so unless you want to play with those, you can ignore them.
 -->
 <!--<br /><br /><br />-->
-<br>
-	<!-- Create db button -->
-	<form action=\"#\" method=\"post\">
-		<input name=\"create_db\" type=\"submit\" value=\"Create / Reset Database\">
-		" . tokenField() . "
-	</form>
-	<br />
 	<hr />
 </div>";
 
